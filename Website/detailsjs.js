150dc83b7e1571ff3f81beb431dc34ee65c672e2
R-k-o29 <rkojha2908@gmail.com>
2024-04-03 01:12:58 +0530

    Update detailsjs.js

--- Website/detailsjs.js
+++ Website/detailsjs.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to fetch sensor data from NodeMCU
+  // Function to fetch sensor data from NodeMCU via Fiddler proxy
   async function fetchSensorData() {
     try {
       // Fetch temperature data
       const temperatureResponse = await fetch(
-        "http://192.168.117.92/temperature"
+        "http://localhost:8888/http://192.168.117.92/temperature"
       );
       const temperatureData = await temperatureResponse.text();
 
       // Fetch water level data
       const waterLevelResponse = await fetch(
-        "http://192.168.117.92/waterLevel"
+        "http://localhost:8888/http://192.168.117.92/waterLevel"
       );
       const waterLevelData = await waterLevelResponse.text();
 
       // Fetch toxicity data
-      const toxicityResponse = await fetch("http://192.168.117.92/toxicity");
+      const toxicityResponse = await fetch(
+        "http://localhost:8888/http://192.168.117.92/toxicity"
+      );
       const toxicityData = await toxicityResponse.text();
 
       // Fetch tilt data
-      const tiltResponse = await fetch("http://192.168.117.92/tilt");
+      const tiltResponse = await fetch(
+        "http://localhost:8888/http://192.168.117.92/tilt"
+      );
       const tiltData = await tiltResponse.text();
 
       // Return fetched sensor data
